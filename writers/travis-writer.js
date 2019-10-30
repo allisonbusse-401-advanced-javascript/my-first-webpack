85ecb05b99653dca6ec54b930856606fe6587ceb
allisonbusse <allisonbusse@gmail.com>
2019-10-29 17:02:35 -0700

    fixed error

--- writers/travis-writer.js
+++ writers/travis-writer.js
@@ -3,6 +3,6 @@ const { write } = require('./writer');
 function writeTravis(path) {
   const str = `language: node_js
   node_js: node`;
-  write(str, `${path}/travis.yml`);
+  write(str, `${path}/.travis.yml`);
 }
 module.exports = writeTravis;
